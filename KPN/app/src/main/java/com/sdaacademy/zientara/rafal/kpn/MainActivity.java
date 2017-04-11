8e1fc8d74555a16d00e3fcc83aef2c200db38603
Rafal Zientara <rafal.zientara92@gmail.com>
2017-04-11 20:35:24 +0200

    game logic update

--- KPN/app/src/main/java/com/sdaacademy/zientara/rafal/kpn/MainActivity.java
+++ KPN/app/src/main/java/com/sdaacademy/zientara/rafal/kpn/MainActivity.java
@@ -1,9 +1,12 @@
 package com.sdaacademy.zientara.rafal.kpn;
 
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -16,10 +19,12 @@ public class MainActivity extends AppCompatActivity {
     private Drawable stoneDrawable;
 
     private ImageView myAction;
-    private ImageView computerAction;
-    private Action myActionEnum;
-    private Action computerActionEnum;
+    private ImageView computerActionImage;
+    private ActionEnum myActionEnum;
+    private ActionEnum computerActionEnum;
     private Random random;
+    private int myScore;
+    private int computerScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,41 +38,106 @@ public class MainActivity extends AppCompatActivity {
         stoneDrawable = ContextCompat.getDrawable(this, R.drawable.ic_brightness_1_black_24dp);
 
         myAction = (ImageView) findViewById(R.id.myActionImage);
-        computerAction = (ImageView) findViewById(R.id.computerActionImage);
+        computerActionImage = (ImageView) findViewById(R.id.computerActionImage);
     }
 
     public void myClick(View view) {
         switch (view.getId()) {
             case R.id.cutButton:
                 myAction.setImageDrawable(cutDrawable);
-                myActionEnum = Action.CUT;
+                myActionEnum = ActionEnum.CUT;
                 break;
             case R.id.paperButton:
                 myAction.setImageDrawable(paperDrawable);
-                myActionEnum = Action.PAPER;
+                myActionEnum = ActionEnum.PAPER;
                 break;
             case R.id.stoneButton:
                 myAction.setImageDrawable(stoneDrawable);
-                myActionEnum = Action.STONE;
+                myActionEnum = ActionEnum.STONE;
                 break;
         }
 
         computerActionEnum = getRandomAction();
+        setComputerImage();
+        GameResult gameResult = getGameResult();
+        addScore(gameResult);
+        checkScore();
     }
 
-    private Action getRandomAction() {
+    private void checkScore() {
+        if (myScore == 3)
+            showAlertDialog("I win!");
+        else if (computerScore == 3)
+            showAlertDialog("I loose! :(");
+    }
+
+    private void showAlertDialog(String message) {
+        //// TODO: 11.04.2017 AlertDialog.Builder
+
+        AlertDialog alertDialog = new AlertDialog.Builder(this)
+                
+                .create();
+
+        alertDialog.show();
+    }
+
+    private void addScore(GameResult gameResult) {
+        if (gameResult.equals(GameResult.WIN))
+            myScore++;
+        if (gameResult.equals(GameResult.LOSE))
+            computerScore++;
+    }
+
+    private GameResult getGameResult() {
+        if (myActionEnum.equals(computerActionEnum))
+            return GameResult.TIE;
+        if (isMyWin())
+            return GameResult.WIN;
+        return GameResult.LOSE;
+    }
+
+    private boolean isMyWin() {
+        return (myActionEnum.equals(ActionEnum.CUT) && computerActionEnum.equals(ActionEnum.PAPER)) ||
+                (myActionEnum.equals(ActionEnum.PAPER) && computerActionEnum.equals(ActionEnum.STONE)) ||
+                (myActionEnum.equals(ActionEnum.STONE) && computerActionEnum.equals(ActionEnum.CUT));
+    }
+
+    private void setComputerImage() {
+        Drawable computerDrawable = getDrawableForAction(computerActionEnum);
+        computerActionImage.setImageDrawable(computerDrawable);
+    }
+
+    private ActionEnum getRandomAction() {
         int randomAction = random.nextInt(3);
-        if(randomAction == 0)
-            return Action.STONE;
-        if(randomAction == 1)
-            return Action.PAPER;
-        return Action.CUT;
+        if (randomAction == 0)
+            return ActionEnum.STONE;
+        if (randomAction == 1)
+            return ActionEnum.PAPER;
+        return ActionEnum.CUT;
     }
 
-    enum Action {
+    private Drawable getDrawableForAction(ActionEnum action) {
+        switch (action) {
+            default:
+            case CUT:
+                return cutDrawable;
+            case PAPER:
+                return paperDrawable;
+            case STONE:
+                return stoneDrawable;
+        }
+    }
+
+    enum ActionEnum {
         STONE,
         PAPER,
         CUT;
+    }
+
+    enum GameResult {
+        WIN,
+        LOSE,
+        TIE;
     }
 
 }

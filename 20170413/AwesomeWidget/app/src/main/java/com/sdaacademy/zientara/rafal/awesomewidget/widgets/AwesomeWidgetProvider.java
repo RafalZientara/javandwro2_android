7cf5cc6416d5a93681c2da912f8d1ced7493f930
Rafal Zientara <rafal.zientara92@gmail.com>
2017-04-13 20:55:26 +0200

    widget

--- 20170413/AwesomeWidget/app/src/main/java/com/sdaacademy/zientara/rafal/awesomewidget/widgets/AwesomeWidgetProvider.java
+++ 20170413/AwesomeWidget/app/src/main/java/com/sdaacademy/zientara/rafal/awesomewidget/widgets/AwesomeWidgetProvider.java
@@ -1,10 +1,17 @@
 package com.sdaacademy.zientara.rafal.awesomewidget.widgets;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.RemoteViews;
+
+import com.sdaacademy.zientara.rafal.awesomewidget.R;
+
+import java.util.Random;
 
 /**
  * Created by Evil on 13.04.2017.
@@ -25,6 +32,38 @@ public class AwesomeWidgetProvider extends AppWidgetProvider {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
         Log.d(TAG, "onUpdate");
 
+        ComponentName thisWidget = new ComponentName(context, AwesomeWidgetProvider.class);
+
+        int[] widgetsIds = appWidgetManager.getAppWidgetIds(thisWidget);
+        for (int widgetId : widgetsIds) {
+            updateWidget(context, appWidgetManager, appWidgetIds, widgetId);
+        }
+
+
+    }
+
+    private void updateWidget(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds, int widgetId) {
+        int random = new Random().nextInt(1000);
+
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
+                R.layout.widget_layout);
+        remoteViews.setTextViewText(R.id.randomText, String.valueOf(random));
+
+        setButtonAction(context, appWidgetIds, remoteViews);
+
+        appWidgetManager.updateAppWidget(widgetId, remoteViews);
+    }
+
+    private void setButtonAction(Context context, int[] appWidgetIds, RemoteViews remoteViews) {
+        //tworzymy widget aktualizujący widok
+        Intent intent = new Intent(context, AwesomeWidgetProvider.class);
+        intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+
+        //broadcast by wysłać intent do WSZYSTKICH naszych widgetow
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+                0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        remoteViews.setOnClickPendingIntent(R.id.randomText, pendingIntent);
     }
 
     @Override
